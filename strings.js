6c0a44857bd22b38c14ec69e7c7ea269c6cb69eb
Achal <achalddave@live.com>
2013-02-26 17:34:44 -0800

    use jsnode instead because delaynode doesnt work properly with guitars

--- strings.js
+++ strings.js
@@ -3,83 +3,43 @@ function StringNote (frequency) {
   this.frequency = typeof frequency === "undefined" ? 440 : frequency;
   this.context = context;
   this.delaySamples = (context.sampleRate / this.frequency);
-}
-
-StringNote.prototype.setupFilters = function () {
-  // delay
-  var delayNode = this.context.createDelayNode(); // delay filter
-  var delaySeconds = 1/this.frequency; // delaySamples/context.sampleRate
-  delayNode.delayTime.value = delaySeconds;
-  
-  // lowpass
-  var lowpassFilter = context.createBiquadFilter();
-  lowpassFilter.type = lowpassFilter.LOWPASS;
-  lowpassFilter.frequency.value = 20000; // make things sound better
-
-  // gain
-  var gainNode =  context.createGainNode();
-  gainNode.gain.value = 0.996; // mostly for string to die off quicker
-
-  this.lowpassFilter = lowpassFilter;
-  this.delayNode = delayNode;
-  this.gainNode = gainNode;
-
+  this.node = context.createJavaScriptNode(4096, 0, 1);
+  this.N = context.sampleRate / this.frequency;
   this.playing = false;
-}
 
-StringNote.prototype.setupBuffer = function () {
-  // noise
-  
-  // bufferSize just needs to be larger than the delay
-  var bufferSize = 2048;
-
-  var buffer = context.createBuffer(1, bufferSize, context.sampleRate);
-  var bufferSource = context.createBufferSource();
-  bufferSource.buffer = buffer;
-
-  var delaySamples = this.delaySamples;
-  if (delaySamples > bufferSize) {
-    alert("I can't play that frequency...");
-    return;
-  }
-  var bufferData = buffer.getChannelData(0);
-  for (var i = 0; i < delaySamples+1; i++) {
-    bufferData[i] = 2*(Math.random()-0.5); // random noise
-  }
-
-  this.bufferSource = bufferSource;
 }
 
 StringNote.prototype.play = function () {
-  this.setupFilters();
-  this.setupBuffer(); // noise
+  var self = this,
+      N = Math.round(this.N),
+      signal = new Float32Array(N),
+      signalOffset = 0,
+      noiseOffset = 0,
+      noise = 0;
 
-  var context = this.context,
-      destination = context.destination,
-      delayNode = this.delayNode,
-      lowpassFilter = this.lowpassFilter,
-      gainNode = this.gainNode,
-      bufferSource = this.bufferSource;
+  this.node.onaudioprocess = function(e) {
+    var output = e.outputBuffer.getChannelData(0);
+
+    for (var i = 0; i < e.outputBuffer.length; i++) {
+      if (noise < N) {
+        output[i] = signal[signalOffset] = 2*(Math.random() - 0.5);
+        noise++;
+      } else {
+        output[i] = signal[signalOffset] = (signal[signalOffset] + signal[(signalOffset+1) % N]) / 2;
+      }
+      // wrap around if necessary
+      signalOffset = (signalOffset+1) % N;
+    }
+  }
+
   // see diagram: http://upload.wikimedia.org/wikipedia/commons/9/9d/Karplus-strong-schematic.png
-  bufferSource.connect(destination);
-  bufferSource.connect(delayNode);
-  delayNode.connect(lowpassFilter);
-  lowpassFilter.connect(gainNode);
-  gainNode.connect(delayNode);
-  gainNode.connect(destination);
 
-  bufferSource.noteOn(0);
+  this.node.connect(this.context.destination);
   this.playing = true;
 }
 
 StringNote.prototype.pause = function () {
   this.playing = false;
-  this.bufferSource.noteOff(0);
-  
-  // disconnect all the things
-  this.delayNode.disconnect();
-  this.lowpassFilter.disconnect();
-  this.gainNode.disconnect();
 }
 
 StringNote.prototype.setFrequency = function (frequency) {
